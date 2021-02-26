6fe3e0c593bee8cd438aeda163921229beeab1bb
sawsan <3818891+sawsan23@users.noreply.github.com>
2021-02-27 00:12:42 +0630

    ddos.py

--- ddos.py
+++ ddos.py
@@ -20,8 +20,6 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "Author   : ss"
-print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
